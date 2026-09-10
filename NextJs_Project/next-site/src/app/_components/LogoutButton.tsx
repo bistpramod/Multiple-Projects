3ecd1bee3afeacc0a499072b47c0a6143669a94f
bistpramod <bistpramod113@gmail.com>
2026-09-10 19:21:11 +0545

    update and auth due

---
+++ NextJs_Project/next-site/src/app/_components/LogoutButton.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+const LogoutButton =()=>{
+    return <div>Logout page</div>
+
+}
+
+export default LogoutButton
--- NextJs_Project/next-site/src/app/layout.tsx
+++ NextJs_Project/next-site/src/app/layout.tsx
@@ -1,16 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+import Navbar from "./_components/Navbar";
 
 export const metadata: Metadata = {
   title: "Contact Manager",
@@ -19,11 +10,13 @@ export const metadata: Metadata = {
 
 export default function RootLayout({ children }: LayoutProps<"/">) {
   return (
-    <html
-      lang="en"
-      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
-    >
-      <body className="min-h-full flex flex-col">{children}</body>
+    <html lang="en" className={" h-full antialiased"}>
+      <body>
+        <div>
+          <Navbar/>
+          <main>{children} </main>
+        </div>
+      </body>
     </html>
   );
 }
