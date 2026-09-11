ee9e7800587c707d6b1eae1e87ed6b4582644600
bistpramod <bistpramod113@gmail.com>
2026-09-11 23:31:36 +0545

    update

--- NextJs_Project/next-site/src/app/_components/LogoutButton.tsx
+++ NextJs_Project/next-site/src/app/_components/LogoutButton.tsx
@@ -1,24 +1,32 @@
 "use client";
 
 import React from "react";
-import { logoutAction } from "../actions/auth"; 
-const LogoutButton = () => {
-  const handleLogout = async () => {
-   try{
+import { logoutAction } from "../actions/auth";
+import { useRouter } from "next/navigation";
 
-     await logoutAction();
-     // redirect  happens in the server action 
-     // this  client side redirect is a fallback
-   }
-   catch(error){
-    
-    
-   }
+const LogoutButton = () => {
+  const router = useRouter();
+
+  const handleLogout = async () => {
+    try {
+      await logoutAction();
+      // redirect happens in the server action
+      // this client side redirect is a fallback
+      router.push("/login");
+      router.refresh();
+    } catch (error) {
+      console.log("something went wrong ", error);
+    }
   };
 
   return (
     <div>
-      <button onClick={handleLogout}  className="px-4 py-5 bg-red-500 text-white rounded-md hover:bg-red-500 transition-colors cursor-pointer ">Logout</button>
+      <button
+        onClick={handleLogout}
+        className="px-4 py-5 bg-red-500 text-white rounded-md hover:bg-red-500 transition-colors cursor-pointer "
+      >
+        Logout
+      </button>
     </div>
   );
 };
